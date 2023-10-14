6c3effcf01b240a43490f6d9ddf0527ec44de753
Samuel Liebert <Samuelliebert@gmail.com>
2023-10-13 19:53:50 -0500

    changed dir

--- start.py
+++ start.py
@@ -4,6 +4,6 @@ from put_in_ap_mode import put_in_ap_mode
 abs_dir = os.path.abspath("./")
 
 with open("/etc/xdg/lxsession/LXDE-pi/autostart", "w") as file:
-    file.write(f"@sudo {abs_dir}/start_api.sh")
+    file.write(f"@sudo {abs_dir}/start_api.bash {abs_dir}")
 
 put_in_ap_mode()
